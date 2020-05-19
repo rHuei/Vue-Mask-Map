5a64fff6ef751cc9f291c68064ebccc13e9f6b9e
MtFeather <st904583114@gmail.com>
2020-05-19 22:09:56 +0800

    vue first learning v1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row no-gutters">
+      <!-- 選擇所在區域 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="cityName" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <!-- 選擇縣市選單 -->
+            <select id="cityName" class="form-control" v-model="select.city">
+              <option value="">請選擇縣市</option>
+              <option :value="c.CityName" v-for="c in cityName" :key="c.CityName">
+                {{ c.CityName }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="area" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <!-- 選擇地區選單 -->
+            <select id="area" class="form-control" v-model="select.area">
+              <option value="">請選擇地區</option>
+              <option :value="a.AreaName"
+                v-for="a in cityName.find((city) => city.CityName === select.city).AreaList"
+                :key="a.AreaName">
+                {{ a.AreaName }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示藥局位置 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// 導入內部檔案
+import L from 'leaflet';
+import cityName from './assets/cityName.json';
+
+// 設定空物件
+let openStreetMap = {};
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data: () => ({
+    data: [],
+    cityName,
+    select: {
+      city: '臺北市',
+      area: '中正區',
+    },
+  }),
+  computed: {
+    pharmacies() {
+      return this.data.filter((pharmacy) => {
+        if (!this.select.area) {
+          return pharmacy.properties.county === this.select.city;
+        }
+        return pharmacy.properties.town === this.select.area;
+      });
+    },
+  },
+  watch: {
+    // 當選單的值發生變化，就會執行 updateMap(); 方法更新地圖標記
+    pharmacies() {
+      this.updateMap();
+    },
+  },
+  methods: {
+    updateMap() {
+      // clear markers
+      openStreetMap.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          openStreetMap.removeLayer(layer);
+        }
+      });
+
+      this.pharmacies.forEach((pharmacy) => {
+        // 透過藥局經緯度疊加標記
+        L.marker([
+          pharmacy.geometry.coordinates[1],
+          pharmacy.geometry.coordinates[0],
+        ]).addTo(openStreetMap).bindPopup(`<p><strong style="font-size: 20px;">${pharmacy.properties.name}</strong></p>
+          <strong style="font-size: 16px; color: #d45345;">口罩剩餘：成人 - ${pharmacy.properties.mask_adult ? `${pharmacy.properties.mask_adult} 個` : '未取得資料'} / 兒童 - ${pharmacy.properties.mask_child ? `${pharmacy.properties.mask_child} 個` : '未取得資料'}</strong><br>
+          地址: ${pharmacy.properties.address}<br>
+          電話: ${pharmacy.properties.phone}<br>
+          <small>最後更新時間: ${pharmacy.properties.updated}</small>`);
+      });
+    },
+  },
+  mounted() {
+    const api = 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json';
+    this.$http.get(api).then((response) => {
+      // 將空陣列 data 指定為遠端獲得的資料，資料僅需取得藥局資訊即可
+      this.data = response.data.features;
+    });
+
+    openStreetMap = L.map('map', {
+      center: [25.042474, 121.513729],
+      // 可以嘗試改變 zoom 的數值
+      // 筆者嘗試後覺得 18 的縮放比例是較適當的查詢範圍
+      zoom: 18,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 20,
+    }).addTo(openStreetMap);
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'bootstrap/scss/bootstrap';
+
+#map {
+  position: relative;
+  height: 100vh;
 }
 </style>
